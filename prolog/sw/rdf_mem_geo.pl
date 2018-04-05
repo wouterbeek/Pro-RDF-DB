1ffd6bf45c9c4f25a4be11695a0d39dff9f20a41
Wouter Beek <wouter@triply.cc>
2018-04-05 20:56:58 +0200

    ENHANCED: Extend rdf_assert_triple/[3,4] with WKT literal support.
    CLEANUP: Assert all used RDF prefix declarations.
    
    DOC: Improve predicate header documentation.
    
    CLEANUP: Use the newly assed WKT literal hooks to simplify code.

--- prolog/sw/rdf_mem_geo.pl
+++ prolog/sw/rdf_mem_geo.pl
@@ -20,12 +20,18 @@ Multifile hooks do not work (workaround: module prefix).
 */
 
 :- use_module(library(dcg)).
-:- use_module(library(geo/wkt_generate)).
+:- use_module(library(gis/wkt)).
 :- use_module(library(sw/rdf_mem)).
 :- use_module(library(sw/rdf_prefix)).
 :- use_module(library(sw/rdf_term)).
 
-:- rdf_assert_prefix(geo, 'http://www.opengis.net/ont/geosparql#').
+:- dynamic
+    rdf_mem:rdf_assert_object_hook/2.
+
+:- multifile
+    rdf_mem:rdf_assert_object_hook/2.
+
+:- maplist(rdf_assert_prefix, [geo,rdf]).
 
 :- rdf_meta
    rdf_assert_wkt(r, +, r),
@@ -33,12 +39,17 @@ Multifile hooks do not work (workaround: module prefix).
    rdf_triple_wkt(r, ?),
    rdf_triple_wkt(r, ?, r).
 
+rdf_mem:rdf_assert_object_hook(Shape, literal(type(D,Lex))) :-
+  is_wkt_shape(Shape), !,
+  atom_phrase(wkt_generate(Shape), Lex),
+  rdf_equal(D, geo:wktLiteral).
 
 
 
 
-%! rdf_assert_wkt(+Feature, +Shape, +G) is det.
-%! rdf_assert_wkt(+Feature, +Shape, +G, -Geometry) is det.
+
+%! rdf_assert_wkt(+Feature:rdf_term, +Shape:compound, +G:rdf_graph) is det.
+%! rdf_assert_wkt(+Feature:rdf_term, +Shape:compound, +G:rdf_graph, -Geometry:iri) is det.
 
 rdf_assert_wkt(Feature, Shape, G) :-
   rdf_assert_wkt(Feature, Shape, G, _).
@@ -47,14 +58,13 @@ rdf_assert_wkt(Feature, Shape, G) :-
 rdf_assert_wkt(Feature, Shape, G, Geometry) :-
   rdf_bnode_iri(Geometry),
   rdf_assert_triple(Feature, geo:hasGeometry, Geometry, G),
-  atom_phrase(wkt_generate(Shape), Lex),
-  rdf_assert_triple(Geometry, geo:asWKT, literal(type(geo:wktLiteral,Lex)), G),
+  rdf_assert_triple(Geometry, geo:asWKT, Shape, G),
   rdf_assert_triple(Geometry, rdf:type, geo:'Geometry', G).
 
 
 
-%! rdf_triple_wkt(?Feature, ?Shape) is nondet.
-%! rdf_triple_wkt(?Feature, ?Shape, ?G) is nondet.
+%! rdf_triple_wkt(?Feature:rdf_term, ?Shape:compound) is nondet.
+%! rdf_triple_wkt(?Feature:rdf_term, ?Shape:compound, ?G:rdf_graph) is nondet.
 
 rdf_triple_wkt(Feature, Shape) :-
   rdf_triple_wkt(Feature, Shape, _).
