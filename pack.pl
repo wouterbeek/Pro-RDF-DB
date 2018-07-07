430b96d4fec8028c968ca3075ceeebcf80062931
Wouter Beek <wouter@triply.cc>
2018-07-07 02:40:27 +0200

    FIXED: Minor typo.

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 home('https://github.com/wouterbeek/prolog_rdf_db').
 keywords(['Linked Data','RDF','Semantic Web']).
 name(prolog_rdf_db).
-requires(prolog_rdf)
+requires(prolog_rdf).
 title('Prolog-based in-memory RDF database.').
 version('0.0.0').
